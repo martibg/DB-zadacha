e2e6a395939af41d390a30bf572cf139b6b235c8
Martin <marti07009@gmail.com>
2025-06-18 05:52:11 +0300

    Add project files.

---
+++ DB zadacha/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DB_zadacha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(293, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(813, 337);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(110, 94);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(140, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(110, 141);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(140, 22);
+            this.txtAge.TabIndex = 1;
+            this.txtAge.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(110, 195);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(140, 22);
+            this.txtClass.TabIndex = 1;
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(110, 253);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(140, 22);
+            this.txtGrade.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 305);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 35);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(200, 305);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 35);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(110, 305);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 35);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(110, 49);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(140, 22);
+            this.txtID.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtID;
+    }
+}
+
---
+++ DB zadacha/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DB_zadacha
+{
+    public partial class Form1 : Form
+    {
+        string connStr = "server=localhost;user=root;database=StudentsDB;port=3306;password= Qwe1209poi!;";
+        MySqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(connStr);
+            LoadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Students";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Грешка при зареждане: " + ex.Message);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs(out int age, out decimal grade)) return;
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Students(Name, Age, Class, AverageGrade) VALUES (@name, @age, @class, @grade)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@age", age);
+                    cmd.Parameters.AddWithValue("@class", txtClass.Text);
+                    cmd.Parameters.AddWithValue("@grade", grade);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Грешка при добавяне: " + ex.Message);
+            }
+        }
+
+        
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtID.Text = row.Cells["ID"].Value.ToString();
+                txtName.Text = row.Cells["Name"].Value.ToString();
+                txtAge.Text = row.Cells["Age"].Value.ToString();
+                txtClass.Text = row.Cells["Class"].Value.ToString();
+                txtGrade.Text = row.Cells["AverageGrade"].Value.ToString();
+            }
+        }   
+        
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtID.Text, out int id))
+            {
+                MessageBox.Show("Невалидно ID.");
+                return;
+            }
+
+            if (!ValidateInputs(out int age, out decimal grade)) return;
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    string query = "UPDATE Students SET Name=@name, Age=@age, Class=@class, AverageGrade=@grade WHERE ID=@id";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@age", age);
+                    cmd.Parameters.AddWithValue("@class", txtClass.Text);
+                    cmd.Parameters.AddWithValue("@grade", grade);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Грешка при редактиране: " + ex.Message);
+            }
+        }
+        
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtID.Text, out int id))
+            {
+                MessageBox.Show("Невалидно ID.");
+                return;
+            }
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    string query = "DELETE FROM Students WHERE ID=@id";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Грешка при изтриване: " + ex.Message);
+            }
+        }
+
+        private void ClearFields()
+        {
+            txtID.Clear();
+            txtName.Clear();
+            txtAge.Clear();
+            txtClass.Clear();
+            txtGrade.Clear();
+        }
+
+        private bool ValidateInputs(out int age, out decimal grade)
+        {
+            age = 0;
+            grade = 0;
+
+            if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtClass.Text))
+            {
+                MessageBox.Show("Име и паралелка са задължителни.");
+                return false;
+            }
+
+            if (!int.TryParse(txtAge.Text, out age))
+            {
+                MessageBox.Show("Невалидна възраст.");
+                return false;
+            }
+
+            if (!decimal.TryParse(txtGrade.Text, out grade))
+            {
+                MessageBox.Show("Невалиден среден успех.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+
+    
+
+
+    
+
